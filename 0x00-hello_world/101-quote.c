425e445076fe67d6cba443a25a6149c12adfc6d4
Krystian Maccs <krystianmaccs@gmail.com>
2022-06-30 16:57:04 -0700

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdio.h>
 
 /**
 * main - Entry point
+*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-<<<<<<< HEAD
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-=======
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
->>>>>>> 60b4de86a836e6508bfd5d2b2066817f93b68606
-return (1);
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+        return (1);
 }
