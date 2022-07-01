77b5413454148071beb2aa7cd649e556e20854bd
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-01 14:17:35 -0700

    if else

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
+#include <ctype.h>
 
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		letter = tolower(letter);
+		putchar(letter);
+		if (letter == 'z')
+		{
+			letter = 'A';
+			for (; letter <= 'Z'; letter++)
+			{
+				putchar(letter);
+			}
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
