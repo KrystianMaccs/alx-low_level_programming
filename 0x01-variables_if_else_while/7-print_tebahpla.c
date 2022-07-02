0577022726f59b6783bda83bcb99a9d45f92ec17
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-01 17:18:04 -0700

    if else

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stadio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit == '9')
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
