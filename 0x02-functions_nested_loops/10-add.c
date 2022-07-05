6197bf92979e6292816bfdbc99801d7ba7cbb9a2
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-05 11:34:17 -0700

    functions

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: 0 Always (Success)
+ * adds integers - this function adds 2 integers and returns the result
+ * Return: 0
  */
 int add(int num1, int num2)
 {
