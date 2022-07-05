8526c807010716be2b29ee194a289a577cf64b0f
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-05 10:31:47 -0700

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,24 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
  * Return: 0 Always (Success)
  */
-int main(void)
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
