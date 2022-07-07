f3d3ab29b2767022ddd0275633bdbedcb1cb7146
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-07 16:18:06 -0700

    fixes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <ctype.h>
+
 /**
  * _isupper - function that checks for uppercase character
  *
  * @c: return char type
  *
- * Return 0 on success and 1 on fail
+ * Return: 0 on success and 1 on fail
  */
 
 int _isupper(int c)
