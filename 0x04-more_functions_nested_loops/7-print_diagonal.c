a18989394a631c9c01d36c0614c2928560021b85
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-07 08:06:34 -0700

    done some tasks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,31 @@
 #include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line in the terminal
+ * @n: integer argument
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int j = 0;
+
+		while (j < n)
+		{
+			int i;
+
+			for (i = 1; i <= j; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			j++;
+		}
+
+	}
+	else
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,29 @@
 #include n"main.h"
+
+/**
+ * print_square - prints a square
+ * @size: integer argument
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int j = 0;
+
+		while (j < size)
+		{
+			int i;
+
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			j++;
+		}
+	}
+	else
+		_putchar('\n');
+}
