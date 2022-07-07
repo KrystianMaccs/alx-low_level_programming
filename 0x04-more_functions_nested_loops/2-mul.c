e8712312317515c269e5fff00572195637854167
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-07 07:29:52 -0700

    task 3

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * mul - function tht multiplies two integers
+ *
+ * @a: an integer type
+ * @b: another integer
+ *
+ * Return: 0 on success and 1 on fail
+ */
+
+int mul(int a, int b)
+{
+	int m = a * b;
+
+	return (m);
+}
