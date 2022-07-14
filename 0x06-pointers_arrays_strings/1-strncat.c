f62f1c3ae8359562758b55a6e2dbec2a06b72c57
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-14 11:24:41 -0700

    fixes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,18 +13,15 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 	i = 0;
 
-	while (dest[i] != '\0')
+	while (*(dest + i))
 	{
 		i++;
 	}
-	j = 0;
-
-	while (src[j] != '\0' && j < n)
+	while (*(src + j) != '\0' && j < n)
 	{
-		dest[i] = src[j];
+		*(dest + i) = src[j];
 		i++;
 		j++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
