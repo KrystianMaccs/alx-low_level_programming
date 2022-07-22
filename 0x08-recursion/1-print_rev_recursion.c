a7b1932fb6f538283924ce4049919131b0efabf4
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-21 17:05:07 -0700

    recursion problems

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,7 +4,7 @@
  * _print_rev_recursion - Prints a string in reverse
  * @s: The string to print
  */
-void _print_rev_recusrion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
