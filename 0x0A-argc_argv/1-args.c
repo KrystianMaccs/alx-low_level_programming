1947fff3e593a82637a2039e82610aa64043f4b5
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-28 07:46:52 -0700

    fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - Prints the name of the program
- * @argc: Count arguments
- * @argv: Arguments to be counted
- * Return: Always 0 (Success)
- */int main(int argc, char *argv[])
+ *  main - print the name of the program
+ *  @argc: Count arguments
+ *  @argv: Arguments
+ *  Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
 {
-	(void) argv; /* Ignore argv */
-	printf("%i\n", argc - 1);
+(void) argv; /*Ignore argv*/
+printf("%i\n", argc - 1);
 
-	return (0);
+return (0);
 }
