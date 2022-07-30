8230204af8e0cbed5f520df2f40959f352a34492
Krystian Maccs <krystianmaccs@gmail.com>
2022-07-29 17:30:27 -0700

    1- strdup task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,27 +1,42 @@
 #include "main.h"
+#include <stdlib.h>
+
 
 /**
- * _strdup -> string duplicator function
+ * _strdup - string duplicator function
  * @str: string to be duplicated
  * Return: a string pointer
+ *
+ * Return: NULL if str in NULL
+ * pointer to duplicated string on success
+ * NULL if memory was insufficient
  */
 char *_strdup(char *str)
 {
-	int i = 1, j = 0;
-	char *s;
-
+	unsigned int len, i;
+	char *nstr;
+	/* check if str is null */
 	if (str == NULL)
-		return (NULL);
-	while (str[i])
-		i++;
-	s = (char *) malloc(i * sizeof(char) + 1);
-	if (s == NULL)
-		return (NULL);
-	while (j < i)
 	{
-		s[j] = str[j];
-		j++;
+		return (NULL);
 	}
-	s[j] = '\0';
-	return (s);
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	nstr = malloc(sizeof(char) * (len + 1));
+
+	/* checks if malloc was successful */
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		nstr[i] = str[i];
+	}
+	nstr[len] = '\0';
+	return (nstr);
 }
