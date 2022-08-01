2173fcc5f1e76906c6a3861384be9721b56e2ff7
Krystian Maccs <krystianmaccs@gmail.com>
2022-08-01 08:27:14 -0700

    fixes

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,10 @@
 
 /**
  * main - print the name of the program compile from,followed by a new line
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
-
