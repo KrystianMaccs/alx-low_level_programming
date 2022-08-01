9dfc7f60c2d75bc1ad79aafad3fceb665047c21d
Krystian Maccs <krystianmaccs@gmail.com>
2022-08-01 08:20:15 -0700

    preprocessor tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program compile from,followed by a new line
+ * return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This repo harbors the preprocessor tasks.
